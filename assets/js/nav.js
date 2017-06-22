d199daa3c0160a230ffd43eda8c6ed52f58299ae
deepmind <creativeautomaton@gmail.com>
2017-06-22 17:31:21 -0500

    fixedmenu and page layouts added mailchimp subscribe and styles

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,7 +3,17 @@ $( document ).ready(function() {
     console.log( "Jquery ready!" );
 
     $('body').on('click', '.hamburger', function(){
-      $(this).toggleClass('is-active');
+        $(this).toggleClass('is-active');
+        $('.nav, .nav-home').toggleClass('slideInRight hide');
+        // $('.hamburger-inner').toggleClass('active');
+
+    });
+    $(document).on('scroll', function(){
+        var stickyHeader = $('html, body').scrollTop();
+        if (stickyHeader >= 100) {
+            console.log('reached 100px ');
+            $('.hamburger-container').addClass('sticky');
+        } else { $('.hamburger-container').removeClass('sticky'); }
     });
 
 });
