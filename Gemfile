04075147ff504c3e26e27998caebf0d9c80f3686
deepmind <creativeautomaton@gmail.com>
2017-07-10 17:31:25 -0500

    latest things

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
 
 gem 'github-pages'
-gem 'html-proofer'
+gem 'html-proofer' 
 # gem 'jekyll-admin', group: :jekyll_plugins
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -7,6 +7,7 @@ $( document ).ready(function() {
          dataType: "json",
          success: function (data){
               siteData(data);
+              // localstorage.setitem(data);
          },
          error: function (error){
                console.log('failed...');
@@ -19,13 +20,60 @@ $( document ).ready(function() {
              console.log(data);
          }, 200);
     }
-    setTimeout(function(){
-      $('.headline').removeClass('hide').addClass('slideInDown animated');
 
-    }, 500);
-   setTimeout(function(){
-      $('.headline').addClass('shrink').slow(400); 
-   }, 5000);
+     var heroSeen = localStorage.getItem("heroanimation");
+     console.log(heroSeen);
+    if (heroSeen == null) {
+      console.log(heroSeen);
+      localStorage.setItem("heroanimation", "false");
+    }
+    if (heroSeen = false ) {
+          $('.call-to-action').hide().removeClass('hide');
+
+          setTimeout(function(){
+             $('.logo').removeClass('hide').addClass('three-sec slideInDown animated');
+             $(".title").each(function (index) {
+                 var characters = $(this).text().split("");
+                 $this = $(this);
+                 $this.empty();
+                 $.each(characters, function (i, el) {
+                    $this.append("<p class='slideInLeft animated'>" + el + "</p>");
+                 });
+                  console.log(characters);
+             });
+             $('.title p').each(function() {
+                $(this).hide();
+                var letterVal = $(this).text();
+                if(letterVal == " "){
+                    $(this).css({'margin-left': '2rem'});
+                }
+             });
+          }, 1000);
+
+          setTimeout(function(){
+             heroAnimation();
+              localStorage.setItem("heroanimation", "true");
+          }, 6000);
+
+          function heroAnimation(){
+             $('.title').removeClass('hide');
+             $('#hero .container').addClass('done');
+
+            $('.title p').each(function() {
+                   var time = 4;
+                   $(this).delay(500 * time).show(1000);
+            });
+          }
+           setTimeout(function(){
+                $('.call-to-action').delay(1500).show(500);
+                $('.hire-us').delay(500).show(1000);
+                $('.join-us').delay(500).show(1000);
+           }, 10000);
+    }
+
+     $('body').on('click', '.testHero', function(){
+         heroAnimation();
+     });
 
     $('body').on('click', '.hamburger', function(){
         $(this).toggleClass('is-active');
