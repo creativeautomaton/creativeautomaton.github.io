99f7dd0173a0bac7f4e8d93794e5b501d65a7802
deepmind <creativeautomaton@gmail.com>
2017-07-21 16:54:33 -0500

    git api testing to get the latest... uh git.

--- _site/assets/js/git.js
+++ _site/assets/js/git.js
@@ -1,8 +1,50 @@
 $( document ).ready(function() {
     console.log( "Git Commit ready!" );
-
     var repoURL ="https://api.github.com/repos/creativeautomaton/creativeautomaton.github.io";
+    var clientID = "Iv1.e7d53c09d763fb72";
+    var clientSecret = "45080ffb34402b42d515b951926f285f0f9a51ac";
+    var accesstoken =  "0edbdfc2c1e20c0ff037d61317977f5213ca9d45";
 
+function getOAuth(clientID, clientSecret){
+      //get authorize
+      console.log('loaded get0auth()  '+ clientID);
+        $.ajax({
+            type: "POST",
+            url: "https://api.github.com/users/",
+            headers: {
+              "username": "creativeautomaton",
+              "password": "zjkhdc00",
+            },
+            accepts: "application/json"
+          }).done(function (data) {
+            console.log('success: ');
+            console.log(data);
+            // code to handle succesful response
+          }).fail(function (data) {
+              console.log('failed: ');
+             console.log(data);
+            // code to handle error response
+        });
+        // $.ajax({
+        //     type: "GET",
+        //     url: "http://github.com/login/oauth/authorize",
+        //     contentType: "application/json",
+        //     dataType: "json",
+        //     data: JSON.stringify(
+        //          {
+        //            "client_id": "Iv1.e7d53c09d763fb72",
+        //            "state": "fvfjhghjfdghdhgdhgfhgf",
+        //            "redirect_uri": "https://www.google.com"
+        //          })
+        //   })
+        //   .fail(function( data ) {
+        //     console.log( data );
+        //   })
+        //   .done(function( data ) {
+        //     console.log( data );
+        //   });
+}
+getOAuth(clientID, clientSecret);
 function getSHA(repoURL){
       //get branch SHA
         $.ajax({
@@ -17,12 +59,36 @@ function getSHA(repoURL){
             getCommit(sha);
           });
 }
+
+function createBlob(repoURL, accesstoken){
+  // Create a Blob
+
+      $.ajax({
+          type: "POST",
+          url: repoURL + "/blobs",
+          contentType: "application/json",
+          dataType: "json",
+          data: JSON.stringify({
+              "content": "Content of the blob",
+              "encoding": "utf-8"
+          }),
+          headers: { 'Authorization': accesstoken },
+        })
+        .done(function( data ) {
+          console.log( data );
+        }).fail(function (data) {
+            console.log('failed: ');
+           console.log(data);
+          // code to handle error response
+      });
+}
+// createBlob(repoURL);
 function getCommit(sha){
       $.ajax({
           type: "GET",
           url: repoURL + "/git/commits/" + sha,
           contentType: "application/json",
-          dataType: "json",
+          dataType: "json"
         })
         .done(function( data ) {
           console.log( data );
@@ -48,26 +114,37 @@ function createCommit(parentsArray, treeSHA, author, repoURL, sha ){
              url: repoURL + "/git/commits/" + sha,
              contentType: "application/json",
              dataType: "json",
-             data: JSON.stringify({
-                   "message": "wtf boom!",
-                   "author": {
-                     "name": author.name,
-                     "email": author.email,
-                     "date": author.date
-                   },
-                   "parents": [
-                     parentsArray
-                   ],
-                   "tree": treeSHA
-              })
+             data: JSON.stringify(
+                  {
+                    "message": "my commit message",
+                    "author": {
+                      "name": "Scott Chacon",
+                      "email": "schacon@gmail.com",
+                      "date": "2008-07-09T16:13:30+12:00"
+                    },
+                    "parents": [
+                      parentsArray
+                    ],
+                    "tree": treeSHA
+                  }
+              // {
+              //      "message": "wtf boom!",
+              //      "author": {
+              //        "name": author.name,
+              //        "email": author.email,
+              //        "date": author.date
+              //      },
+              //      "parents": [
+              //        parentsArray
+              //      ],
+              //      "tree": treeSHA
+              // }
+              )
            })
-          .error( function(error){
-          console.log(error);
-          })
            .done(function( data ) {
              console.log( data );
            });
 }
 
- getSHA(repoURL);
+ // getSHA(repoURL);
 });
